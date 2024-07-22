20192f159288d592b2b670a93adb286bea46452e
maratreason <maratreason@mail.ru>
2024-07-22 12:16:30 +0300

    feat(users) add ngrx

--- apps/rooi/src/app/app.config.ts
+++ apps/rooi/src/app/app.config.ts
@@ -1,10 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import {ApplicationConfig, provideZoneChangeDetection} from "@angular/core";
+import {provideRouter} from "@angular/router";
+import {appRoutes} from "./app.routes";
+import {provideStore, provideState} from "@ngrx/store";
+import {provideEffects} from "@ngrx/effects";
+import * as fromUsers from "../../../../libs/rooi/users/data-access/src/lib/+state/users.reducer";
+import {UsersEffects} from "../../../../libs/rooi/users/data-access/src/lib/+state/users.effects";
+import {UsersFacade} from "../../../../libs/rooi/users/data-access/src/lib/+state/users.facade";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideEffects(UsersEffects),
+    provideState(fromUsers.USERS_FEATURE_KEY, fromUsers.usersReducer),
+    UsersFacade,
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(appRoutes),
   ],
 };
